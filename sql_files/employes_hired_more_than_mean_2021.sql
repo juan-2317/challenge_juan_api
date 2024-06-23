cc8abd62d076057eb477d0f9bef3ca6f3374102c
Juan Pablo Reina <juan.reingut@hotmail.com>
2024-06-23 11:43:59 -0500

    Se modifican los SELECT de SQL para que sean vistas y facilitar el mantenimiento a futuro

--- sql_files/employes_hired_more_than_mean_2021.sql
+++ sql_files/employes_hired_more_than_mean_2021.sql
@@ -1,22 +1,28 @@
-WITH employees_hired_2021 AS (
-	SELECT b.id AS department_id, b.department, COUNT(DISTINCT a.id) AS hired
-	FROM `hired_employees` a
-	INNER JOIN `departments` b
-	ON a.department_id = b.id
-	WHERE EXTRACT(YEAR FROM a.datetime) = 2021
-	GROUP BY department_id, department
-), hired_after_2021 AS (
-	SELECT b.id AS department_id, b.department,
-	COUNT(DISTINCT a.id) AS hired
-	FROM `hired_employees` a
-	INNER JOIN `departments` b
-	ON a.department_id = b.id
-	WHERE EXTRACT(YEAR FROM a.datetime) > 2021 OR a.datetime IS NULL
-	GROUP BY department_id, department
-)
-SELECT a.*
-FROM hired_after_2021 a
-LEFT JOIN employees_hired_2021 b
-ON a.department_id = b.department_id AND a.department = b.department
-WHERE a.hired > b.hired
-ORDER BY a.hired DESC;
+CREATE OR REPLACE VIEW employees_hired_more_than_mean_2021 AS 
+	WITH employees_hired_2021 AS (
+		SELECT
+			b.id AS department_id,
+			b.department,
+			COUNT(DISTINCT a.id) AS hired
+		FROM `hired_employees` a
+		INNER JOIN `departments` b
+		ON a.department_id = b.id
+		WHERE EXTRACT(YEAR FROM a.datetime) = 2021
+		GROUP BY department_id, department
+	), hired_after_2021 AS (
+		SELECT
+			b.id AS department_id,
+			b.department,
+			COUNT(DISTINCT a.id) AS hired
+		FROM `hired_employees` a
+		INNER JOIN `departments` b
+		ON a.department_id = b.id
+		WHERE EXTRACT(YEAR FROM a.datetime) > 2021 OR a.datetime IS NULL
+		GROUP BY department_id, department
+	)
+	SELECT a.*
+	FROM hired_after_2021 a
+	LEFT JOIN employees_hired_2021 b
+	ON a.department_id = b.department_id AND a.department = b.department
+	WHERE a.hired > b.hired
+	ORDER BY a.hired DESC;
