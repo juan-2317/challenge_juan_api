7c9070a20cc909e1e8a2e47132cab60ad1200374
Juan Pablo Reina <juan.reingut@hotmail.com>
2024-06-23 11:24:52 -0500

    La fecha de contratacion no siempre registra, se modifica la tabla para que acepte nulos

--- sql_files/departments.sql
+++ sql_files/departments.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `challengeglobant`.`departments` (
 	`id` INT(11) NOT NULL COMMENT 'Id of the department' ,
-	`department` VARCHAR(255) NOT NULL COMMENT 'Name of the department' ,
+	`department` VARCHAR(255) COMMENT 'Name of the department' ,
 	PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
